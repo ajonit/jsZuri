c7744be0ad63e4dc8820ce02002739a2965a70a5
AJ <ankurjain@duck.com>
2022-06-20 23:01:18 +0530

    Basic Stats

---
+++ basicstats.js
@@ -0,0 +1,4 @@
+const myName = "Ankur Jain";
+var myheight = 178;
+var myCountry = "India";
+console.log("My name is " + myName + "; My height is " + myheight + "cm; " + "My country is " + myCountry);
